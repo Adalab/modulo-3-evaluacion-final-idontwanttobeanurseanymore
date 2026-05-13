a700a273fd8824d930853efc36ca17792091051c
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-05-13 20:32:35 +0200

    placeholder solved

--- src/components/CharacterItem.jsx
+++ src/components/CharacterItem.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router';
 //, ${new Date().getFullYear() - characterObj.birthYear} años
-export default function CharacterItem({ characterObj }) {
+export default function CharacterItem({ characterObj, findCharacter }) {
+  const characterImg =
+    'https://dummyimage.com/210x295/ffffff/656565.png&text=Harry+Potter';
   return (
     <Link to={`/detalle/${characterObj.id}`} className=''>
       <img
         className='characterImg'
-        src={`${characterObj.image ? characterObj.image : 'desconocida'}`}
+        src={`${characterObj.image ? characterObj.image : characterImg}`}
         alt={`Foto de ${characterObj.name}`}
         title={`Foto de ${characterObj.name}`}
       ></img>
