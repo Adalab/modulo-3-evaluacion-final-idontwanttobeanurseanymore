14b5cf91919712b641073ae9104e4d8774571849
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-05-14 18:59:44 +0200

    detail page design

--- src/components/CharacterDetailPage.jsx
+++ src/components/CharacterDetailPage.jsx
@@ -5,66 +5,68 @@ export default function CharacterDetailPage({ findCharacter }) {
   const characterFound = findCharacter(params.id);
   const dummyCharacterImg =
     'https://dummyimage.com/210x295/ffffff/656565.png&text=Harry+Potter';
+  const housesImg = {
+    imgGryffindor: '/harry-potter-gryffindor.avif',
+    imgSlytherin: '/harry-potter-slytherin.avif',
+    imgRavenclaw: '/harry-potter-ravenclaw.avif',
+    imgHufflepuff: '/harry-potter-hufflepuff.avif',
+  };
   let gender;
   let isItAlive;
   if (characterFound.gender) {
     if (characterFound.gender === 'male') {
       gender = 'masculino';
       isItAlive = 'vivo';
+    } else if (characterFound.gender === 'female') {
+      gender = 'femenino';
+      isItAlive = 'viva';
+    } else {
+      gender = 'desconocido';
     }
-  } else if (characterFound.gender === 'female') {
-    gender = 'femenino';
-    isItAlive = 'viva';
-  } else {
-    gender = 'desconocido';
   }
 
-  // console.log(characterFound);
   return (
-    <article className={`house${characterFound.house}`}>
+    <article className={` detailCard house${characterFound.house}`}>
+      <Link to='/' className='backLink'>
+        Volver
+      </Link>
       <img
-        className='characterImg'
+        className='characterImgDetail'
         src={`${characterFound.image ? characterFound.image : dummyCharacterImg}`}
         alt={`Foto de ${characterFound.name}`}
         title={`Foto de ${characterFound.name}`}
       ></img>
-      <section>
-        <h2>{characterFound.name}</h2>
-        <div>
-          <p>Casa: {characterFound.house}</p>
-          <p>Estudiante: {characterFound.student ? 'Si' : 'No'}</p>
-          <p>Género: {gender}</p>
+
+      <section className='detailContent'>
+        <h2 className='detailName'>{characterFound.name}</h2>
+        <hr />
+        <div class='houseRow'>
+          <p>{characterFound.house}</p>
+          <img
+            className='housesImg'
+            src={housesImg[`img${characterFound.house}`]}
+            alt='house'
+            alt={`Escudo de la casa ${characterFound.house} `}
+            title={`Escudo de la casa ${characterFound.house} `}
+          ></img>
+        </div>
+        <hr />
+        <div className='gridInfo'>
           <p>
-            Fecha de nacimiento: {characterFound.birthDate || 'desconocida'}
+            Género: <span>{gender}</span>
           </p>
           <p>Estatus: {isItAlive ? isItAlive : 'Sin vida'}</p>
-          <p>
-            Especie:{' '}
-            {characterFound.specie ? characterFound.specie : 'desconocida'}
-          </p>
+          <p>Estudiante: {characterFound.student ? 'Si' : 'No'}</p>
+          <p>Fecha de nacimiento {characterFound.birthDate || 'desconocida'}</p>
+
           <p>
             Intérprete:{' '}
             {characterFound.actor ? characterFound.actor : 'desconocido'}
           </p>
         </div>
-        <Link to='/'>Volver</Link>
       </section>
     </article>
   );
 }
 {
-  /**   return {
-              id: characterObj.id,
-              name: characterObj.name,
-              house: characterObj.house,
-              isItAlive: characterObj.alive,
-              gender: characterObj.gender,
-              specie: characterObj.species,
-              alternativeName: characterObj.alternate_names,
-              image: characterObj.image,
-              actor: characterObj.actor,
-              student: characterObj.hogwartsStudent,
-              birthYear: parseInt(characterObj.hogwartsStudent.yearOfBirth),
-            };
-            new Date().getFullYear() - character.birthYear */
 }
