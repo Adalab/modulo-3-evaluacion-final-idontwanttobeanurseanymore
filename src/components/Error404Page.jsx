67ab86bd4daf9cc7c24d4e9906029e12ce6a68e6
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-05-18 18:09:30 +0200

    error 404 changes

--- src/components/Error404Page.jsx
+++ src/components/Error404Page.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router';
+import Hogwarts from '../assets/icon-hogwarts.png';
+import LondonTrain from '../assets/london.jpg';
 
 export default function Error404Page() {
+  const hogwartsImg = Hogwarts;
+  const trainImg = LondonTrain;
   return (
     <article className='error404Page'>
-      <h1>¡Ups!</h1>
-      <p>No hemos podido encontrar la página que buscas</p>
+      <h1 className='error404Pagetitle'>¡Ups!</h1>
+      <p className='error404PageContent'>
+        No hemos podido encontrar la página que buscas
+      </p>{' '}
+      <Link to='/'>
+        <div className='error404Link'>
+          <img src={trainImg} className='error404PageImg2'></img>
 
-      <Link to='/'>Volver a la página de inicio</Link>
+          <p className='error404TextLink'>Volver</p>
+        </div>
+      </Link>
     </article>
   );
 }
